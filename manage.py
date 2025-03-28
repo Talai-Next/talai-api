910eaa7aab2ecddacb43332c559f640069250167
Nantawat Sukrisunt <nantawat.suk@ku.th>
2025-03-28 20:45:35 +0700

    set port to 8080

--- manage.py
+++ manage.py
@@ -17,6 +17,9 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+from django.core.management import execute_from_command_line
+import os
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    port = os.getenv('DJANGO_PORT', '8080')
+    execute_from_command_line(['manage.py', 'runserver', f'127.0.0.1:{port}'])
